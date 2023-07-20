7ed22f1c3369df9166faaa04c86b4f28580575bf
Parker Johnson <parkerj667@gmail.com>
2023-07-20 16:00:06 -0700

    Uploading for assignment 7

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'text', 'created_date')
+    fieldsets = (
+        ('Posts', {
+            'fields': ('title', 'author', 'text')
+        }),
+        )
+    inlines = [CategoryInline]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+
